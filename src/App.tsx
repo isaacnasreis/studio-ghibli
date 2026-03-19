9b39f426cae9897528ac768b7c765ab4ef02ea78
Isaac Reis <isaacreisn@gmail.com>
2026-03-19 19:17:05 -0300

    feat: definindo estrutura basica do conteudo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,19 @@ function App() {
     <>
       <NavBar />
       <section id="center">
-        <h1>TESTE</h1>
+        <div>
+          <h2>Hayao Miyazaki</h2>
+          <h3>A Viagem de Chihiro</h3>
+          <p>
+            Chihiro chega a um mundo mágico dominado por uma bruxa. Aqueles que
+            a desobedecem são transformados em animais.
+          </p>
+          <div>
+            <button>Assista Agora</button>
+            <button>Assista o Trailer</button>
+          </div>
+        </div>
+        <div>Fantasmas</div>
       </section>
     </>
   );
