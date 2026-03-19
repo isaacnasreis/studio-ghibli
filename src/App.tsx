cc7b5c12814d15d5492ff3c4f8cb58b2791e824a
Isaac Reis <isaacreisn@gmail.com>
2026-03-19 19:39:53 -0300

    feat: fazendo mais estruturas de testos para a pagina inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
+import style from "./App.module.css";
 import NavBar from "./components/NavBar/Index";
 
 function App() {
   return (
     <>
       <NavBar />
-      <section id="center">
-        <div>
-          <h2>Hayao Miyazaki</h2>
-          <h3>A Viagem de Chihiro</h3>
-          <p>
+      <section className={style.principal} id="center">
+        <div className={style.content}>
+          <h2 className={style.titleSecondary}>Hayao Miyazaki</h2>
+          <h3 className={style.title}>A Viagem de Chihiro</h3>
+          <p className={style.textPrimary}>
             Chihiro chega a um mundo mágico dominado por uma bruxa. Aqueles que
             a desobedecem são transformados em animais.
           </p>
-          <div>
+          <div className={style.buttons}>
             <button>Assista Agora</button>
             <button>Assista o Trailer</button>
           </div>
